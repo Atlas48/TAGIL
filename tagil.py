52281a86d99f4a682251372ba8fd591598da0c27
AtlasJan <sonikong@gmail.com>
2014-05-01 16:47:24 +0100

    Update TAGIL.py
    formatted a few things

--- tagil.py
+++ tagil.py
@@ -6,7 +6,7 @@
 #it under the terms of the GNU General Public License as published by
 #the Free Software Foundation, either version 3 of the License, or
 #(at your option) any later version.
-
+#
 #This program is distributed in the hope that it will be useful,
 #but WITHOUT ANY WARRANTY; without even the implied warranty of
 #MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
@@ -19,10 +19,11 @@
 #Code by Archie Sullivan
 
 class tagil
-    #Start Quasi-Namespace:
+    #Start "Namespace":
     opmsg=""	
-    #End Quasi-Namespace
+    #End "Namespace"
     def start
         print(opmsg)
+
         #End start
     
